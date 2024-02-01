dc75e65b4051d5b24b0070c77226b925fc5ac4b6
kaloyan <kaloyan.totev1@gmail.com>
2024-02-01 14:31:22 +0200

    added gps utility class

--- raspberry_ws/src/navigation/navigation/GPS.py
+++ raspberry_ws/src/navigation/navigation/GPS.py
@@ -6,6 +6,7 @@ import rclpy
 from rclpy.node import Node
 from custom_msgs.msg import Detection
 from std_msgs.msg import String
+from navigation.GPSUtil import GPSUtil
 
 
 class Gps(Node):
@@ -20,7 +21,13 @@ class Gps(Node):
         port = "/dev/ttyAMA0"
         self.ser = serial.Serial(port, baudrate=9600, timeout=0.5)
         self.dataout = pynmea2.NMEAStreamReader()
-
+        self.util = GPSUtil()
+        # Set frame points
+        top_left_point = (42.851781, 27.800468)
+        top_right_point = (42.850520, 27.879634)
+        bottom_right_point = (42.789835, 27.884055)
+        bottom_left_point = (42.801670, 27.790202)
+        trajectory_utility.update_frame(top_left_point, top_right_point, bottom_right_point, bottom_left_point)
 
     def timer_callback(self):
         msg = Detection()
@@ -32,12 +39,11 @@ class Gps(Node):
             lat = newmsg.latitude
             lng = newmsg.longitude
             gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+            self.util.update_current_location((lat, lng))
             print(gps)
-        #self.publisher_.publish(msg)
-        #self.get_logger().info('\n\r CENTRAL_PUB: "%s" \n\r' % msg)
-        #self.i += 1
-
-
+        # self.publisher_.publish(msg)
+        # self.get_logger().info('\n\r CENTRAL_PUB: "%s" \n\r' % msg)
+        # self.i += 1
 
 
 def main(args=None):
